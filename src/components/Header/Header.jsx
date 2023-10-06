8234d56c991ada511ff41f0aba06cb8cced2d2a7
Mahedee Hasan <mahedee007@gmail.com>
2023-10-07 04:23:29 +0600

    header add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <nav className="header">
+            <img src="/src/images/Logo.svg" alt="" />
+            <div className="nav-menu">
+                <a href="/order">Order</a>
+                <a href="/order-review">Order Review</a>
+                <a href="/management">Management Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+
+        </nav>
     );
 };
 
