277da37a6cd5fcf2ea9a9f85055c9f1d6fe1f67d
Mahedee Hasan <mahedee007@gmail.com>
2023-10-21 23:48:48 +0600

    new section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className="header">
             <img src="/src/images/Logo.svg" alt="" />
             <div className="nav-menu">
-                <a href="/order">Order</a>
-                <a href="/order-review">Order Review</a>
-                <a href="/management">Management Inventory</a>
-                <a href="/login">Login</a>
+                <Link to="/">Shop</Link>
+                <Link to="/order">Order</Link>
+                <Link to="/order-review">Order Review</Link>
+                <Link to="/management">Management Inventory</Link>
+                <Link to="/login">Login</Link>
             </div>
 
         </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+import Home from './components/Layout/Home.jsx';
+import Shop from './components/Shop/Shop.jsx';
+import Order from './components/Order/Order.jsx';
+import OrderReview from './components/OrderReview/OrderReview.jsx';
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>
+      },
+      {
+        path: "/order",
+        element: <Order></Order>
+      },
+      {
+        path: "/order-review",
+        element: <OrderReview></OrderReview>
+      },
+      {
+        path: "/management",
+        element: <Inventory></Inventory>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
